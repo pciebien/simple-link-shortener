873435a42e3e0c8ca54db6815eb2f5f6fe8ab3db
Patryk Ciebień <cyberitymotion@outlook.com>
2019-05-04 22:18:48 +0200

    Workflow testing

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <cmath>
 
 namespace lsl {}
+namespace environment {}
+namespace tools {}
 
 using namespace std;
 using namespace lsl;
@@ -23,15 +25,42 @@ namespace environment
     };
 }
 
+namespace tools
+{
+    class base_converter
+    {
+    public:
+        static string encode(int value)
+        {
+            string characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            vector<char> *encodedPartials = new vector<char>();
+
+            while (value > 0)
+            {
+                int val = value % characters.size();
+                encodedPartials->push_back(characters[val]);
+                value = (int)floor(value / (float)characters.size());
+            }
+
+            string encodedIndex = "";
+            for(int i = 0; i < encodedPartials->size(); ++i)
+                encodedIndex += (*encodedPartials)[i];
+
+            delete encodedPartials;
+            return encodedIndex;
+        }
+    };
+}
+
 namespace lsl
 {
     class link
     {
     public:
-        link(string origin, int index)
+        link(string origin, string shortened)
         {
             _origin = origin;
-            _shortened = shorten(index);
+            _shortened = shortened;
         }
 
         bool originEqual(string link)
@@ -44,29 +73,19 @@ namespace lsl
             return _shortened == link;
         }
 
+        string getOrigin()
+        {
+            return _origin;
+        }
+
+        string getShortened()
+        {
+            return _shortened;
+        }
+
     private:
         string _origin;
         string _shortened;
-
-        string shorten(int index)
-        {
-            string characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            vector<char> *encodedPartials = new vector<char>();
-
-            while (index > 0)
-            {
-                int val = index % characters.size();
-                encodedPartials->push_back(characters[val]);
-                index = (int)floor(index / (float)characters.size());
-            }
-
-            string encodedIndex = "";
-            for(int i = 0; i < encodedPartials->size(); ++i)
-                encodedIndex += (*encodedPartials)[i];
-
-            delete encodedPartials;
-            return encodedIndex;
-        }
     };
 
     class link_collection
@@ -77,6 +96,33 @@ namespace lsl
             _links = new vector<link*>();
         }
         
+        string *getOrigins()
+        {
+            string *pOrigins = new string[_links->size()];
+
+            for(int i = 0; i < _links->size(); ++i)
+                pOrigins[i] = (*_links)[i]->getOrigin();
+
+            return pOrigins;
+        }
+
+        string *getShortenedLinks()
+        {
+            string *pShortenedLinks = new string[_links->size()];
+
+            for(int i = 0; i < _links->size(); ++i)
+                pShortenedLinks[i] = (*_links)[i]->getShortened();
+
+            return pShortenedLinks;
+        }
+
+        void add(string origin, string *shortenedLink)
+        {
+            (*shortenedLink) = tools::base_converter::encode(_links->size() + 1);
+            link *pLink = new link(origin, (*shortenedLink)); 
+            _links->push_back(pLink);            
+        }
+
         ~link_collection()
         {
             delete _links;
@@ -92,6 +138,17 @@ int main()
     environment::env_data *env = new environment::env_data();
     link_collection *links = new link_collection();
 
-    delete env, links;
+    string shortenedLink;
+    links->add("https://google.com", &shortenedLink);
+    
+    string *pOrigins = links->getOrigins();
+    string *pShortenedLinks = links->getShortenedLinks();
+    
+    for(int i = 0; i < pOrigins->size(); ++i)
+    {
+        cout << (*pOrigins)[i] << "    " << (*pShortenedLinks)[i] << endl;
+    }
+
+    delete env, links, pOrigins, pShortenedLinks;
     return 0;
 }
